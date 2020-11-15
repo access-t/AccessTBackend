2518123c0a0d8b271d1473cbba8f5f2ef5abccc7
root <root@ip-172-31-24-101.us-east-2.compute.internal>
2020-11-15 01:09:36 +0000

    deploy

--- src/api.py
+++ src/api.py
@@ -35,4 +35,4 @@ api.add_resource(registration.UserRegistration, "/api/account/create")
 api.add_resource(collections.Collections, "/api/collections")
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(host="0.0.0.0", port="8080", debug=True)
